aab522f28e4e105a5b8b5ec307d5f2aaed8598ff
Lalitha2869 <lalitha.d@truglobal.com>
2026-06-16 19:36:30 +0530

    docker deployment resolved

--- backend/database/db.py
+++ backend/database/db.py
@@ -8,13 +8,31 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
+
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+    if not all([
+        DB_HOST,
+        DB_PORT,
+        DB_NAME,
+        DB_USER,
+        DB_PASSWORD
+    ]):
+        raise ValueError(
+            "Database environment variables missing"
+        )
+
     DATABASE_URL = (
         f"postgresql://"
-        f"{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:"
-        f"{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
+        f"{DB_USER}:"
+        f"{DB_PASSWORD}@"
+        f"{DB_HOST}:"
+        f"{DB_PORT}/"
+        f"{DB_NAME}"
     )
 
 engine = create_engine(DATABASE_URL)
@@ -24,3 +42,5 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+print("DATABASE_URL =", DATABASE_URL)
